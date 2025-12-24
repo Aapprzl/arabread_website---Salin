cc273c69e843511ffa0cbe4d39bf852e8c1fc141
Aapprzl <153051833+Aapprzl@users.noreply.github.com>
2025-12-25 02:26:30 +0800

    update besar

---
+++ assets/data/vocabs.js
@@ -0,0 +1,13 @@
+// assets/data/vocabs.js
+window.VOCABS = [
+  // ===== Sekolah =====
+  { ar:'مَدْرَسَةٌ', id:'Sekolah', tema:'sekolah' },
+  { ar:'مُعَلِّمٌ', id:'Guru', tema:'sekolah' },
+
+  // ===== Keluarga =====
+  { ar:'أَبٌ', id:'Ayah', tema:'keluarga' },
+  { ar:'أُمٌّ', id:'Ibu', tema:'keluarga' },
+
+  // ===== Hewan =====
+  { ar:'قِطٌّ', id:'Kucing', tema:'hewan' },
+];
